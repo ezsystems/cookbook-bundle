4316ff5393aa91bd52e9a91f08cbe2d87d5c51a1
André R. <andre.romcke@gmail.com>
2013-01-31 05:55:44 -0800

    Create FindContent3Command.php
    Adds a search example for how to combine OR and AND as well as using ContentTypeIdentifier

---
+++ Command/FindContent3Command.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * File containing the FindContent3Command class.
+ *
+ * @copyright Copyright (C) 2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace EzSystems\CookbookBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+
+
+/**
+ * This command performs a subtree and content type identifier filter search
+ */
+class FindContent3Command extends ContainerAwareCommand
+{
+    /**
+     * This method override configures on input argument for the content id
+     */
+    protected function configure()
+    {
+        $this->setName( 'ezpublish:cookbook:find_filter' )->setDefinition(
+            array(
+                new InputArgument( 'contentTypeIdentifier', InputArgument::REQUIRED, 'Content type identifier, one or several seperated by comma. example --contentTypeIdentifier=article,folder' ),
+                new InputArgument( 'locationId', InputArgument::REQUIRED, 'Subtree id' ),
+            )
+        );
+    }
+
+    protected function execute( InputInterface $input, OutputInterface $output )
+    {
+        /** @var $repository \eZ\Publish\API\Repository\Repository */
+        $repository = $this->getContainer()->get( 'ezpublish.api.repository' );
+        $searchService = $repository->getSearchService();
+        $locationService = $repository->getLocationService();
+
+        $text = $input->getArgument( 'text' );
+        $contentTypeIdentifierList = explode( ',', $input->getArgument( 'contentTypeId' ) );
+        $locationId = $input->getArgument( 'locationId' );
+
+        // create the query with subtree and a or condition of content type identifiers criteria
+        $query = new \eZ\Publish\API\Repository\Values\Content\Query();
+        $subtreeCriterion = new Criterion\Subtree( $locationService->loadLocation( $locationId )->pathString );
+        $congtentTypeOr = new Criterion\LogicalOr( array() );
+
+        foreach ( $contentTypeIdentifierList as $contentTypeIdentifier )
+            $congtentTypeOr->criteria[] = new Criterion\ContentTypeIdentifier( $contentTypeIdentifier );
+
+        $query->criterion = new Criterion\LogicalAnd(
+            array( $subtreeCriterion, $congtentTypeOr )
+        );
+
+        $result = $searchService->findContent( $query );
+        $output->writeln( '<info>Found ' . $result->totalCount . ' items</info>' );
+        foreach ( $result->searchHits as $searchHit )
+        {
+            $output->writeln( "* " . $searchHit->valueObject->contentInfo->name );
+        }
+    }
+}

dacb1d591eaf45bce858e4eeab8c7b9407bb6428
Andrew Longosz <andrzej.longosz@ez.no>
2017-02-16 10:57:11 +0100

    Fixed case mismatch in CreateXmlContentCommand class name

--- Command/CreateXmlContentCommand.php
+++ Command/CreateXmlContentCommand.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * File containing the CreateXMLContentCommand class.
+ * File containing the CreateXmlContentCommand class.
  *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
- * @version //autogentag//
  */
 namespace EzSystems\CookbookBundle\Command;
 
